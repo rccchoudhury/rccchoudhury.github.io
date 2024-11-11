38f0790e16bc5afd76366215c098dd14152fa58c
Rohan Choudhury <rchoudhu@andrew.cmu.edu>
2024-11-11 18:02:51 +0000

    use swiper js, move videos to main assets directory

--- rlt/static/js/index.js
+++ rlt/static/js/index.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
         $(".navbar-menu").toggleClass("is-active");
     });
 
-    // Initialize Swiper
-    const swiper = new Swiper('.kinetics-swiper', {
+    // Common Swiper configuration
+    const swiperConfig = {
         slidesPerView: 2,
         spaceBetween: 30,
         centeredSlides: true,
@@ -24,8 +24,8 @@ $(document).ready(function () {
         },
         on: {
             slideChange: function () {
-                // Pause all videos
-                const videos = document.querySelectorAll('.swiper-slide video');
+                // Pause all videos in this swiper
+                const videos = this.el.querySelectorAll('.swiper-slide video');
                 videos.forEach(video => {
                     video.pause();
                 });
@@ -50,62 +50,13 @@ $(document).ready(function () {
                 }
             }
         }
+    };
+
+    // Initialize all swipers
+    const kineticsSwiper = new Swiper('.kinetics-swiper', swiperConfig);
+    const ssv2Swiper = new Swiper('.ssv2-swiper', swiperConfig);
+    const resultsSwiper = new Swiper('.results-swiper', {
+        ...swiperConfig,
+        slidesPerView: 1, // Show only one video at a time for the results section
     });
-
-    bulmaSlider.attach();
-})
-
-$(document).ready(function () {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function () {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    });
-
-    // Initialize Swiper
-    const swiper = new Swiper('.kinetics-swiper', {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        centeredSlides: true,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        on: {
-            slideChange: function () {
-                // Pause all videos
-                const videos = document.querySelectorAll('.swiper-slide video');
-                videos.forEach(video => {
-                    video.pause();
-                });
-                
-                // Play the video in the active slide
-                const activeSlide = this.slides[this.activeIndex];
-                if (activeSlide) {
-                    const video = activeSlide.querySelector('video');
-                    if (video) {
-                        video.play();
-                    }
-                }
-            },
-            init: function() {
-                // Play the first video on init
-                const activeSlide = this.slides[this.activeIndex];
-                if (activeSlide) {
-                    const video = activeSlide.querySelector('video');
-                    if (video) {
-                        video.play();
-                    }
-                }
-            }
-        }
-    });
-
-    bulmaSlider.attach();
-})
+});
