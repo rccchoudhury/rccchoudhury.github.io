a580b0baae98232de80297a00d3c647b76cb0afd
Rohan Choudhury <rchoudhu@andrew.cmu.edu>
2024-11-11 17:52:32 +0000

    use swiper JS

--- rlt/static/js/index.js
+++ rlt/static/js/index.js
@@ -6,42 +6,54 @@ $(document).ready(function () {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-
     });
 
-    var options = {
-        slidesToScroll: 1,
-        slidesToShow: 2,
+    // Initialize Swiper
+    const swiper = new Swiper('.kinetics-swiper', {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        centeredSlides: true,
         loop: true,
-        infinite: true,
-        autoplay: false,
-        autoplaySpeed: 3000,
-    }
-
-    // Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for (var i = 0; i < carousels.length; i++) {
-        // Add listener to  event
-        carousels[i].on('before:show', state => {
-            console.log(state);
-        });
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-        // bulmaCarousel instance is available as element.bulmaCarousel
-        element.bulmaCarousel.on('before-show', function (state) {
-            console.log(state);
-        });
-    }
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        on: {
+            slideChange: function () {
+                // Pause all videos
+                const videos = document.querySelectorAll('.swiper-slide video');
+                videos.forEach(video => {
+                    video.pause();
+                });
+                
+                // Play the video in the active slide
+                const activeSlide = this.slides[this.activeIndex];
+                if (activeSlide) {
+                    const video = activeSlide.querySelector('video');
+                    if (video) {
+                        video.play();
+                    }
+                }
+            },
+            init: function() {
+                // Play the first video on init
+                const activeSlide = this.slides[this.activeIndex];
+                if (activeSlide) {
+                    const video = activeSlide.querySelector('video');
+                    if (video) {
+                        video.play();
+                    }
+                }
+            }
+        }
+    });
 
     bulmaSlider.attach();
-
 })
-
 
 $(document).ready(function () {
     // Check for click events on the navbar burger icon
@@ -49,38 +61,51 @@ $(document).ready(function () {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-
     });
 
-    var options = {
-        slidesToScroll: 2,
-        slidesToShow: 2,
+    // Initialize Swiper
+    const swiper = new Swiper('.kinetics-swiper', {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        centeredSlides: true,
         loop: true,
-        infinite: true,
-        autoplay: false,
-        autoplaySpeed: 3000,
-    }
-
-    // Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel_two', options);
-
-    // Loop on each carousel initialized
-    for (var i = 0; i < carousels.length; i++) {
-        // Add listener to  event
-        carousels[i].on('before:show', state => {
-            console.log(state);
-        });
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-        // bulmaCarousel instance is available as element.bulmaCarousel
-        element.bulmaCarousel.on('before-show', function (state) {
-            console.log(state);
-        });
-    }
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        on: {
+            slideChange: function () {
+                // Pause all videos
+                const videos = document.querySelectorAll('.swiper-slide video');
+                videos.forEach(video => {
+                    video.pause();
+                });
+                
+                // Play the video in the active slide
+                const activeSlide = this.slides[this.activeIndex];
+                if (activeSlide) {
+                    const video = activeSlide.querySelector('video');
+                    if (video) {
+                        video.play();
+                    }
+                }
+            },
+            init: function() {
+                // Play the first video on init
+                const activeSlide = this.slides[this.activeIndex];
+                if (activeSlide) {
+                    const video = activeSlide.querySelector('video');
+                    if (video) {
+                        video.play();
+                    }
+                }
+            }
+        }
+    });
 
     bulmaSlider.attach();
-
 })
